14aea0aa62cc3cde4f28e7f518e41bcd48d0d31a
MadnessJ <63245839+MadnessJ@users.noreply.github.com>
2025-11-29 22:46:28 +0700

    update

---
+++ Assets/Scripts/Audio_Manager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Audio_Manager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+[Header("Audio Clips")]
+    public AudioClip background;
+    public AudioClip walking;
+}
